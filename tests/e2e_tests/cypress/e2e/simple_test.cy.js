39c1c9007c92f5ad39af409013eca15158543b26
Krzysztof <krzysztof.kolodzinski@lifeflow.eu>
2022-09-30 01:53:03 +0200

    added more meaningful test

--- tests/e2e_tests/cypress/e2e/simple_test.cy.js
+++ tests/e2e_tests/cypress/e2e/simple_test.cy.js
@@ -1,16 +1,29 @@
-describe('the most basic test', () => {
+function posts() {
+  return cy.get('.row.align-items-center')
+}
+
+describe('Blog test', () => {
   beforeEach(() => {
-    cy.visit('http://whichmailing.localhost:5000/')
+    cy.visit('/')
   })
 
-  it('displays two posts on blog', () => {
-    cy.get('.row.align-items-center').should('have.length', 2)
-
-  })
-
-  it('clicking blog post in list returns blog post page', () => {
-    cy.get('.row.align-items-center').contains('title').click()
+  it('display posts and leave comment in one of them', () => {
+    posts()
+      .should('have.length', 2)
+      .contains('title').click()
     cy.contains('content')
+
+    let user = 'commenting user'
+    let comment = 'comment content'
+
+    cy.get('#author_name').type(user)
+    cy.get('#comment').type(comment)
+    cy.get('#submit').click()
+
+    cy.get('.table.table-striped')
+      .contains(user)
+    cy.get('.table.table-striped')
+      .contains(comment)
   })
 
   it('clicking tag it filters posts with tag', () => {
@@ -18,10 +31,3 @@ describe('the most basic test', () => {
     cy.get('.post-title').should('have.length', 1)
   })
 })
-
-
-//
-//    def test_blog(self):
-//        self.driver.find_element(By.LINK_TEXT, 'BLOG').click()
-//        self.driver.find_element(By.PARTIAL_LINK_TEXT, 'tag/1').click()
-//        self.assertEqual("Blog - tag: tag/1 - Which Mailing", self.driver.title, "webpage title is not matching")
