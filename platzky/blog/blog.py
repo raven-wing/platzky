28c8c5939745d673ede15fb3110a4af86393831e
Krzysztof <krzysztof.kolodzinski@lifeflow.eu>
2023-01-18 15:39:45 +0100

    more tests for blog

--- platzky/blog/blog.py
+++ platzky/blog/blog.py
@@ -1,5 +1,5 @@
-from flask import request, render_template, redirect, send_from_directory,\
-    make_response, url_for, Blueprint, Markup
+from flask import request, render_template, send_from_directory,\
+    make_response, Blueprint, Markup
 from platzky.blog import comment_form, post_formatter
 from os.path import dirname
 
@@ -31,28 +31,30 @@ def create_blog_blueprint(db, config, babel):
         response.headers["Content-Type"] = "application/xml"
         return response
 
-    @blog.route('/<post_slug>', methods=["GET", "POST"])
-    def get_post(post_slug):
-        if request.method == "POST":
-            comment = request.form.to_dict()
-            db.add_comment(post_slug=post_slug, author_name=comment["author_name"],
-                           comment=comment["comment"])
-            return redirect(url_for('blog.get_post', post_slug=post_slug, comment_sent=True, language=locale()))
+    @blog.route('/<post_slug>', methods=["POST"])
+    def post_comment(post_slug):
+        comment = request.form.to_dict()
+        db.add_comment(post_slug=post_slug, author_name=comment["author_name"],
+                       comment=comment["comment"])
+        return get_post(post_slug=post_slug)
 
+    @blog.route('/<post_slug>', methods=["GET"])
+    def get_post(post_slug):
         if raw_post := db.get_post(post_slug):
-            return render_template("post.html", post=post_formatter.format_post(raw_post), post_slug=post_slug,
+            formatted_post = post_formatter.format_post(raw_post)
+            return render_template("post.html", post=formatted_post, post_slug=post_slug,
                                    form=comment_form.CommentForm(), comment_sent=request.args.get('comment_sent'))
         else:
             return page_not_found("no such page")
 
-    @blog.route('/page/<path:page_slug>', methods=["GET", "POST"])
-    def get_page(page_slug):
-        if post := db.get_page(page_slug):
-            if cover_image := post.get("coverImage"):
+    @blog.route('/page/<path:page_slug>', methods=["GET"])
+    def get_page(page_slug):  # TODO refactor to share code with get_post since they are very similar
+        if page := db.get_page(page_slug):
+            if cover_image := page.get("coverImage"):
                 cover_image_url = cover_image["url"]
             else:
                 cover_image_url = None
-            return render_template("page.html", post=post, cover_image=cover_image_url)
+            return render_template("page.html", page=page, cover_image=cover_image_url)
         else:
             return page_not_found("no such page")
 
